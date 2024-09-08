5edcff94880c179941df6a87a8325b7ba8015cc1
Chang Luo <33987852+luochang212@users.noreply.github.com>
2024-09-09 03:07:42 +0800

    first commit

---
+++ utils.py
@@ -0,0 +1,174 @@
+import time
+import torch
+from torch import nn
+import sklearn.metrics
+import matplotlib.pyplot as plt
+from IPython import display
+
+
+size = lambda x, *args, **kwargs: x.numel(*args, **kwargs)
+
+
+class Timer:
+    """记录多次运行时间"""
+    def __init__(self):
+        """Defined in :numref:`subsec_linear_model`"""
+        self.times = []
+        self.start()
+
+    def start(self):
+        """启动计时器"""
+        self.tik = time.time()
+
+    def stop(self):
+        """停止计时器并将时间记录在列表中"""
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+
+    def avg(self):
+        """返回平均时间"""
+        return sum(self.times) / len(self.times)
+
+    def sum(self):
+        """返回时间总和"""
+        return sum(self.times)
+
+    def cumsum(self):
+        """返回累计时间"""
+        return np.array(self.times).cumsum().tolist()
+
+
+class Animator:
+    """在动画中绘制数据"""
+    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
+                 ylim=None, xscale='linear', yscale='linear',
+                 fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
+                 figsize=(3.5, 2.5)):
+        # 增量地绘制多条线
+        if legend is None:
+            legend = []
+        self.use_svg_display()
+        self.fig, self.axes = plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [self.axes, ]
+        # 使用lambda函数捕获参数
+        self.config_axes = lambda: self.set_axes(
+            self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+        self.X, self.Y, self.fmts = None, None, fmts
+        
+    @staticmethod
+    def use_svg_display():
+        """Use the svg format to display a plot in Jupyter.
+
+        Defined in :numref:`sec_calculus`"""
+        from matplotlib_inline import backend_inline
+        backend_inline.set_matplotlib_formats('svg')
+    
+    @staticmethod
+    def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
+        """Set the axes for matplotlib.
+
+        Defined in :numref:`sec_calculus`"""
+        axes.set_xlabel(xlabel), axes.set_ylabel(ylabel)
+        axes.set_xscale(xscale), axes.set_yscale(yscale)
+        axes.set_xlim(xlim),     axes.set_ylim(ylim)
+        if legend:
+            axes.legend(legend)
+        axes.grid()
+
+    def add(self, x, y):
+        # 向图表中添加多个数据点
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        self.config_axes()
+        display.display(self.fig)
+        display.clear_output(wait=True)
+
+
+class Accumulator:
+    """在n个变量上累加"""
+    def __init__(self, n):
+        self.data = [0.0] * n
+
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+def accuracy(y_hat, y):
+    """计算预测正确的数量"""
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
+
+
+def evaluate_accuracy_gpu(net, data_iter, device=None):
+    """使用GPU计算模型在数据集上的精度
+
+    Defined in :numref:`sec_lenet`"""
+    if isinstance(net, nn.Module):
+        net.eval()  # 设置为评估模式
+        if not device:
+            device = next(iter(net.parameters())).device
+    # 正确预测的数量，总预测的数量
+    metric = Accumulator(2)
+    with torch.no_grad():
+        for X, y in data_iter:
+            if isinstance(X, list):
+                # BERT微调所需的（之后将介绍）
+                X = [x.to(device) for x in X]
+            else:
+                X = X.to(device)
+            y = y.to(device)
+            metric.add(accuracy(net(X), y), size(y))
+    return metric[0] / metric[1]
+
+
+def try_gpu(i=0):
+    """如果存在，则返回gpu(i)，否则返回cpu()"""
+    if torch.cuda.device_count() >= i + 1:
+        return torch.device(f'cuda:{i}')
+    return torch.device('cpu')
+
+
+def eval_multi(
+    y_true,
+    y_label,
+    average_method='macro'
+):
+    """
+    Evaluate a binary classification task.
+    """
+
+    # Metrics that require the predicted labels (y_label)
+    acc = sklearn.metrics.accuracy_score(y_true=y_true, y_pred=y_label)
+    precision = sklearn.metrics.precision_score(y_true=y_true, y_pred=y_label, average=average_method)
+    recall = sklearn.metrics.recall_score(y_true=y_true, y_pred=y_label, average=average_method)
+    f1 = sklearn.metrics.f1_score(y_true=y_true, y_pred=y_label, average=average_method)
+    cm = sklearn.metrics.confusion_matrix(y_true=y_true, y_pred=y_label)
+
+    print(f'accuracy: {acc:.4f}')
+    print(f'precision: {precision:.4f}')
+    print(f'recall: {recall:.4f}')
+    print(f'f1_score: {f1:.4f}')
+    print(f'confusion matrix:\n{cm}')
